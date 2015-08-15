998f119f4536679a5e293e104c803cb17558eda5
Nathan Berliner <nathan.k.berliner@gmail.com>
2015-08-14 21:22:15 -0500

    more on details of fxn

--- R/Trace.R
+++ R/Trace.R
@@ -2,10 +2,11 @@
 #' 
 #' @param x a square matrix
 #' @return the trace of the matrix
-#' @details Can you believe that this is not available as a function in base?
-#'   This function computes the trace of a matrix by first getting the diagonal
+#' @details Can you believe that this is not available as a function in base? 
+#'   This function computes the trace of a matrix by first getting the diagonal 
 #'   elements, and then summming.
 #' @aliases Trace trace TR tr TRACE
+
 
 
 Tr=function(x){
